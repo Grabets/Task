f31a77d7420b46bac2828bc8bdd78973ae3dc994
Grabets <grabetsb@gmail.com>
2015-10-07 22:59:55 +0300

    update method findEquipmentByCost, main to remove case

--- src/ua/java/climber/Climber.java
+++ src/ua/java/climber/Climber.java
@@ -12,33 +12,30 @@ public class Climber {
     private int age;
     private List<Equipment> equipments;
     private String rank;
-    private int weightLimit;
 
-    public Climber(String name, int age, String rank, List<Equipment> equipments){
+    public Climber(String name, int age, String rank, List<Equipment> equipments) {
         this.name = name;
         this.age = age;
         this.rank = rank;
         this.equipments = equipments;
-        //setWeightLimit(rank);
-        //TODO: check weight limit
+
     }
 
-    public int getCost(){
+    public int getCost() {
         int price = 0;
-        for (Equipment equipment : equipments){
+        for (Equipment equipment : equipments) {
             price += equipment.getCost();
         }
         return price;
     }
 
-    public void sortEquipmentsByWeight(){
-        Collections.sort(equipments, new Comparator() {
-            public int compare(Object o1, Object o2) {
-                Equipment equipment1 = (Equipment) o1;
-                Equipment equipment2 = (Equipment) o2;
-                if (equipment1.getWeight() > equipment2.getWeight())
+    //TODO: lambda?
+    public void sortEquipmentsByWeight() {
+        Collections.sort(equipments, new Comparator<Equipment>() {
+            public int compare(Equipment o1, Equipment o2) {
+                if (o1.getWeight() > o2.getWeight())
                     return 1;
-                if (equipment1.getWeight() < equipment2.getWeight())
+                if (o1.getWeight() < o2.getWeight())
                     return -1;
                 return 0;
             }
@@ -47,58 +44,34 @@ public class Climber {
             System.out.print(equipment);
     }
 
-    public void findEquipmentsByCost(int low, int high){
-        for (Equipment equipment : equipments){
+    public List findEquipmentsByCost(int low, int high) {
+        List<Equipment> eq = new ArrayList<>();
+        for (Equipment equipment : equipments) {
             if (equipment.getCost() > low & equipment.getCost() < high)
-                System.out.print(equipment.toString());
+                eq.add(equipment);
         }
-    }
-
-    public void equip(String... equip){
-        for (int i = 0; i < equip.length; i++){
-            if (fillEquipment(equip[i]) != null)
-                equipments.add(fillEquipment(equip[i]));
+        if (eq.size()==0){
+            System.out.println("No match found");
+            return null;
         }
-    }
-
-    private Equipment fillEquipment(String type) {
-        switch (type) {
-            case "Boots":
-                return new Boots("hiking", 43, 2300, 350);
-            case "Helmet":
-                return new Helmet("plastic", 50, 320, 60);
-            case "Clothes":
-                return new Clothes("full wear", "XL", 5300, 800);
-            case "Rucksack":
-                return new Rucksack(80, 1300, 60);
-            case "IceAxe":
-                return new IceAxe("B", 70, 650, 120);
-            case "Crampon":
-                return new Crampon("attachable", 43, 14, 860, 190);
-            case "Rope":
-                return new Rope(true, 10, 50, 3200, 80);
-            default: {
-                System.out.println("Equipment not matched correctly!");
-                return null;
-            }
-        }
+        else return eq;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder climberToText = new StringBuilder();
         climberToText.append("Name: " + name + ", age: " + age + ", rank: " + rank);
-        climberToText.append("\n"+"Equipments:\n");
+        climberToText.append("\n" + "Equipments:\n");
         for (Equipment equipment : equipments)
             climberToText.append(new StringBuilder(equipment.toString()));
         return new String(climberToText);
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
@@ -106,11 +79,11 @@ public class Climber {
         this.equipments = equipments;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
@@ -118,26 +91,4 @@ public class Climber {
         return equipments;
     }
 
-    public int getWeightLimit() {
-        return weightLimit;
-    }
-
-    private void setWeightLimit(String rank){
-        switch (rank){
-            case "III":
-                this.weightLimit = 10000;
-            case "II":
-                this.weightLimit = 15000;
-            case "I":
-                this.weightLimit = 20000;
-            case "KMS":
-                this.weightLimit = 25000;
-            case "MS":
-                this.weightLimit = 30000;
-            default:{
-                System.out.println("Rank not matched correctly");
-                this.weightLimit = 0;
-            }
-        }
-    }
 }
--- src/ua/java/climber/PrepareToClimb.java
+++ src/ua/java/climber/PrepareToClimb.java
@@ -1,19 +1,31 @@
 package ua.java.climber;
 
+import ua.java.equipment.*;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bogdan on 10/1/2015.
  */
 public class PrepareToClimb {
     public static void main(String[] args) {
-        Climber climber = new Climber("Anatoliy Bukreev", 32, "MS", new ArrayList<>());
-        climber.equip("Clothes", "Boots", "Crampon", "Helmet", "IceAxe", "Rucksack", "Rope");
+        List<Equipment> equip = new ArrayList<>();
+        equip.add(new Boots("hiking", 43, 2300, 350));
+        equip.add(new Helmet("plastic", 50, 320, 60));
+        equip.add(new Clothes("full wear", "XL", 5300, 800));
+        equip.add(new Rucksack(80, 1300, 60));
+        equip.add(new IceAxe("B", 70, 650, 120));
+        equip.add(new Crampon("attachable", 43, 14, 860, 190));
+        equip.add(new Rope(true, 10, 50, 3200, 80));
+
+        Climber climber = new Climber("Anatoliy Bukreev", 32, "MS", equip);
         System.out.println(climber);
         System.out.println("Price of all equipments: " + climber.getCost() + "$");
         System.out.println("\nSorted equipments by weight:");
         climber.sortEquipmentsByWeight();
         System.out.print("\nGet elements of equipment by range of cost:\n");
-        climber.findEquipmentsByCost(100, 200);
+        List<Equipment> equipmentsByCost = climber.findEquipmentsByCost(100, 200);
+        for (Equipment equipment : equipmentsByCost)
+            System.out.print(equipment);
     }
 }
